5f94114af58ae8efd0bca1b1a2962ca744ba095a
Renan Gomes <email@renangomes.com>
2015-06-03 11:35:24 -0300

    Create the base pages

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="<?PHP echo ucfirst($page) ?> - eRanker Starter Kit">
-        <title><?PHP echo ucfirst($page) ?> - eRanker Starter Kit</title>
+        <meta name="description" content="<?PHP echo $page_title ?>">
+        <title><?PHP echo $page_desc ?></title>
 
         <!-- Theme CSS -->
         <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
