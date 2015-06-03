365c50b8cd5e60f00832e36ec1be979b7853dc27
Renan Gomes <email@renangomes.com>
2015-06-03 11:27:44 -0300

    Theme Install

--- footer.php
+++ footer.php
@@ -1,2 +1,10 @@
-<?php
+<!-- JS Libs -->
+<script src="../bower_components/jquery/dist/jquery.min.js"></script>
+<script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="../bower_components/metisMenu/dist/metisMenu.min.js"></script>
 
+<!-- Theme JavaScript -->
+<script src="../bower_components/startbootstrap-sb-admin-2/dist/js/sb-admin-2.js"></script>
+
+</body>
+</html>
