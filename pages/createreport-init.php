10bff8ece7e1a23160f19496ef8a84ba7198cbc6
Renan Gomes <email@renangomes.com>
2015-06-03 11:37:07 -0300

    Create the base pages

---
+++ pages/createreport-init.php
@@ -0,0 +1,6 @@
+<?php
+
+//Overwrite some variables (title and meta description)
+$page_title = "Create Report - eRanker API Starter Kit";
+$page_desc = "Create a new Report on eRanker - eRanker API Starter Kit";
+
