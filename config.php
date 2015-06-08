d11594a742312e5941a5aeea622ddd8aee570f47
Jonas Marinho <email@jonasmarinho.com>
2015-06-08 15:40:56 -0300

    update eranker commons and some images

--- config.php
+++ config.php
@@ -1,5 +1,14 @@
 <?php
 
+//CHANGE THIS SETTINGS - http://www.eranker.com/settings
+$eranker_apikey = "ee9978e1e5280ace6c0a3e99a77ea48c";
+$eranker_apiemail = "renan@georanker.com";
+
+//Required Files
+require_once 'libs/createimage.php';
+require_once 'libs/eRankerAPI.class.php';
+require_once 'libs/eRankerCommons.php';
+
 //Some default variables (can be overwritten by pages init
 $page_title = "Home";
 $page_desc = "This is a DEMO project that uses eRanker API. It contains some basic features like: Create Report; View and print Report; Generate PDF; Latest Reports";
