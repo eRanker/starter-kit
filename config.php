b957971a78f110569f81b7c4183cee2b0b9266a0
Jonas Marinho <email@jonasmarinho.com>
2015-06-08 15:43:38 -0300

    update eranker commons and some images

--- config.php
+++ config.php
@@ -9,7 +9,11 @@ require_once 'libs/createimage.php';
 require_once 'libs/eRankerAPI.class.php';
 require_once 'libs/eRankerCommons.php';
 
-//Some default variables (can be overwritten by pages init
+//Some default variables (can be overwritten by pages init)
 $page_title = "Home";
 $page_desc = "This is a DEMO project that uses eRanker API. It contains some basic features like: Create Report; View and print Report; Generate PDF; Latest Reports";
 $project_name = "eRanker API Starter Kit";
+
+//Init the eranker object
+global $erapi;
+$erapi = new eRankerAPI($eranker_apiemail, $eranker_apikey, true);
